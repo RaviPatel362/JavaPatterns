a342f52ba8bed0b602f45175f9b4e73a060cb371
RAVI <RAVI@LAPTOP-KNRPD1P2>
2020-12-28 19:55:11 +0530

    First Commit:

---
+++ Patterns/src/Pattern5.java
@@ -0,0 +1,18 @@
+
+public class Pattern5 {
+	
+	public static void main(String args []) {
+		
+		
+		for(int i=1;i<=8;i++) 
+		{
+				for(int j=1;j<=i;j++) 
+					{	
+						System.out.print((char)('A' + (j-1)) + " " );
+					}
+				System.out.println();
+		}
+		
+	}
+
+}
